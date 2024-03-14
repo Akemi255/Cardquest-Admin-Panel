0c4a929734685c68c0e233093efa28cf4775cef9
gusvet18 <gus1465@yahoo.com>
2024-03-14 08:28:25 -0500

    cambios responsive

--- src/components/layout/admin-panel.tsx
+++ src/components/layout/admin-panel.tsx
@@ -8,18 +8,18 @@ interface AdminPanelProps {
 export function AdminPanel({children}:AdminPanelProps) {
 
   return (
-    <div className="grid h-screen min-h-screen w-full overflow-hidden lg:grid-cols-[280px_1fr]">
+    <div className="relative h-screen min-h-screen w-full overflow-hidden lg:grid lg:grid-cols-[280px_1fr]">
       <div className="border-r bg-gray-100/40 lg:block dark:bg-gray-800/40">
         <Navbar />
       </div>
-      <div className="flex flex-col">
+      <div className="flex flex-col relative">
         <header className="flex h-14 lg:h-[60px] items-center gap-4 border-b bg-gray-100/40 px-6 dark:bg-gray-800/40">
-          <div className="flex items-center lg:gap-4 lg:ml-auto">
+          <div className="flex items-center gap-4 ml-auto absolute left-6 lg:static">
             <UserMenu />
           </div>
         </header>
         <main className="flex flex-1 flex-col gap-4 p-4">
-         {children}
+          {children}
         </main>
       </div>
     </div>
